d72ec5c9defc4974ecd047b88978894337b41c9e
Rafalspa <spaleniakr@gmail.com>
2023-12-10 12:32:19 +0100

    add index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
+<script setup>
+const open = ref(false);
+const toggle = () => (
+	(open.value = !open.value),
+	(textButton.value = open.value ? "Hide modal" : "Show modal")
+);
+const textButton = ref("Show modal");
+</script>
+
 <template>
 	<h3>To jest strona glowna</h3>
+	<button @click="toggle">{{ textButton }}</button>
+	<div v-if="open">to jest ukryty tekst</div>
 </template>
