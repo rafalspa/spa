2db820c944a5ae56a6b44c301b7b2468887567b7
Rafalspa <spaleniakr@gmail.com>
2023-12-10 13:43:29 +0100

    Otwieranie i zamykanie chatu

--- app.vue
+++ app.vue
@@ -1,8 +1,22 @@
+<script setup>
+const open2 = ref(false);
+const toggle2 = () => (
+	(open2.value = !open2.value),
+	(textButton2.value = open2.value ? "Hide modal" : "Show modal")
+);
+const textButton2 = ref("Show modal");
+</script>
+
 <template>
 	<div>
 		<NuxtLayout>
 			<div class="body-content"><NuxtPage /></div>
-			<div class="kolko"><img src="./img/kolko.png" /></div>
+			<div v-if="open">to jest ukryty tekst</div>
+			<div v-if="open2" class="okienko">
+				to jest okno chatu <button @click="toggle2">X</button>
+				<input type="text" />
+			</div>
+			<div class="kolko" @click="toggle2"><img src="./img/kolko.png" /></div>
 		</NuxtLayout>
 	</div>
 </template>
