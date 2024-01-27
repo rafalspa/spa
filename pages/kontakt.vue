55b16273c77e75a0a2ec7dbd87e7f09fe76e16a0
Rafalspa <spaleniakr@gmail.com>
2024-01-27 18:28:52 +0100

    poprawki stylow

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -10,32 +10,35 @@ useHead({
 </script>
 <template>
 	<h1>Kontakt:</h1>
-	<FormsInput
-		:name="'Podaj imie'"
-		:valInput="''"
-		@sendValue="getData($event)"
-	/>
-	<FormsInput
-		:name="'Podaj nazwisko'"
-		:valInput="''"
-		@sendValue="getData($event)"
-	/>
-	<FormsInput
-		:name="'Podaj e-mail'"
-		:valInput="''"
-		@sendValue="getData($event)"
-	/>
-	<FormsInput
-		:name="'Podaj nr telefonu'"
-		:valInput="''"
-		@sendValue="getData($event)"
-	/>
-	<FormsInput :name="'Temat'" :valInput="''" @sendValue="getData($event)" />
-	<FormsInput
-		:name="'Tresc wiadomosci'"
-		:valInput="''"
-		@sendValue="getData($event)"
-	/>
+	<div style="width: 400px">
+		<FormsInput
+			:name="'Podaj imie'"
+			:valInput="''"
+			@sendValue="getData($event)"
+		/>
+		<FormsInput
+			:name="'Podaj nazwisko'"
+			:valInput="''"
+			@sendValue="getData($event)"
+		/>
+		<FormsInput
+			:name="'Podaj e-mail'"
+			:valInput="''"
+			@sendValue="getData($event)"
+		/>
+		<FormsInput
+			:name="'Podaj nr telefonu'"
+			:valInput="''"
+			@sendValue="getData($event)"
+		/>
+		<FormsInput :name="'Temat'" :valInput="''" @sendValue="getData($event)" />
+		<FormsInput
+			:name="'Tresc wiadomosci'"
+			:valInput="''"
+			@sendValue="getData($event)"
+		/><br />
+		<button class="button is-medium is-link">Wyślij</button>
+	</div>
 </template>
 
 <style scoped lang="scss"></style>
